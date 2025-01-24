eb4e58115809c5485e8dd8862c03e78b65f3b31e
mtliendo <mtliendo@focusotter.com>
2025-01-24 10:35:23 -0600

    updated words

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Features } from '@/components/features'
 import { Testimonials } from '@/components/testimonials'
 import { Pricing } from '@/components/pricing'
 
-export default function Home() {
+export default async function Home() {
 	return (
 		<div className="flex flex-col gap-20 pb-20 max-w-[1400px] mx-auto px-4 sm:px-6 lg:px-8">
 			<Hero />
